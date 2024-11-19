1e98a13f51b481a36320ac55cfdd3302ac2eb773
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2024-11-19 12:04:24 -0500

    Added/started ejs routes...

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('node:path');
 const indexRouter = require('./routes/indexRouter');
 const newMsgRouter = require('./routes/newMsgRouter');
 
 const app = express();
 
+// Views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Routes
+// EJS routes...
+app.get('/', (req, res) => {
+    res.render('index', { message: '' }); // Messages logic....
+});
+
+// Express routes...
 app.use('/', indexRouter); 
 app.use('/new', newMsgRouter);
 
